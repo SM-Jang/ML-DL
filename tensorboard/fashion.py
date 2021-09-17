1cdc4b008b08caafbe678c273f9a9d52375dcecd
SM <smsm8898@ajou.ac.kr>
2021-09-17 14:57:19 +0900

    study

---
+++ tensorboard/fashion.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from model import Net
+from utils import *
+
+# constant for classes
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+
+
+# transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+
+# datasets
+trainset = torchvision.datasets.FashionMNIST('../../data',
+    download=False,
+    train=True,
+    transform=transform)
+testset = torchvision.datasets.FashionMNIST('../../data',
+    download=False,
+    train=False,
+    transform=transform)
+
+# dataloaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                        shuffle=True, num_workers=2)
+
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                        shuffle=False, num_workers=2)
+
+        
+net = Net()        
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+from torch.utils.tensorboard import SummaryWriter
+
+# default `log_dir` is "runs" - we'll be more specific here
+writer = SummaryWriter('runs/fashion_mnist_experiment_1')
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# create grid of images
+img_grid = torchvision.utils.make_grid(images)
+
+# show images
+# matplotlib_imshow(img_grid, one_channel=True)
+
+# write to tensorboard
+writer.add_image('four_fashion_mnist_images', img_grid)
+
+writer.add_graph(net, images)
+writer.close()
+
+
+# select random images and their target indices
+images, labels = select_n_random(trainset.data, trainset.targets)
+
+# get the class labels for each image
+class_labels = [classes[lab] for lab in labels]
+
+# log embeddings
+features = images.view(-1, 28 * 28)
+writer.add_embedding(features,
+                    metadata=class_labels,
+                    label_img=images.unsqueeze(1))
+writer.close()
+
+
+
+running_loss = 0.0
+for epoch in range(1):  # loop over the dataset multiple times
+
+    for i, data in enumerate(trainloader, 0):
+
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        if i % 1000 == 999:    # every 1000 mini-batches...
+
+            # ...log the running loss
+            
+            writer.add_scalar('training loss',
+                            running_loss / 1000,
+                            epoch * len(trainloader) + i)
+
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                            plot_classes_preds(net, inputs, labels),
+                            global_step=epoch * len(trainloader) + i)
+            running_loss = 0.0
+print('Finished Training')
